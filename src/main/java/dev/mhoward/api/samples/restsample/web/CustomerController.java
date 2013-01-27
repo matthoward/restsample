1c6e4501241ea89a28068024443a954101b4b851
Matt Howard <mhoward@axispoint.com>
2013-01-27 18:18:20 -0500

    json/xml complete

--- src/main/java/dev/mhoward/api/samples/restsample/web/CustomerController.java
+++ src/main/java/dev/mhoward/api/samples/restsample/web/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -25,18 +26,22 @@ public class CustomerController {
     CustomerService customerService;
 	
 	@RequestMapping(method=RequestMethod.GET)
+	// @ResponseBody
 	public ModelAndView getAllCustomers(ModelMap model) {
 		List<Customer> customers = customerService.getCustomers();
         CustomerList customerList = new CustomerList(customers);
-        return new ModelAndView("customerView", "customers", customerList);
+        //return customerList;
+        return new ModelAndView("jsonView", "customers", customerList);
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET) 
+	//@ResponseBody
 	public ModelAndView getCustomer(@PathVariable Long id) {
 		 Customer customer = customerService.getCustomer(id);
-	     return new ModelAndView("customerView", "customer", customer);
+		 // return customer;
+	     return new ModelAndView("jsonView", "customer", customer);
 	}
-	
+		
 
     @RequestMapping(method = RequestMethod.POST)
     public View addCustomer(@RequestBody Customer customer)
